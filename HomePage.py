b05c63501b1cec8360f4084f0a915c2e4a252e3d
23365 <23365@students.mrgs.school.nz>
2026-05-04 10:49:12 +1200

    Day 3

--- HomePage.py
+++ HomePage.py
@@ -1,30 +1,37 @@
 import tkinter as tk
+from tkinter import PhotoImage
+from tkinter import Canvas
 
 root = tk.Tk()
 root.title("7 Wonders Of The World Quiz")
+root.geometry("1204x680")
+
+bg = PhotoImage(file="Photos/starting-page.png")
+canvas1 = Canvas(root, width=1204, height=680)
+canvas1.pack(fill="both", expand=True)
+canvas1.create_image(0, 0, image=bg, anchor="nw")
 
 
 label = tk.Label(root, text="7 Wonders Of The World Quiz", font=("Arial", 14, "bold"))
-label.grid(row=0, column=0, columnspan=2, pady=10)
+canvas1.create_window(640, 50, window=label)
 
 
-tk.Label(root, text="First name").grid(row=1, column=0, sticky="e")
-tk.Label(root, text="Last name").grid(row=2, column=0, sticky="e")
+lbl1 = tk.Label(root, text="First name")
+canvas1.create_window(100, 75, window=lbl1)
+ent1 = tk.Entry(root)
+canvas1.create_window(200, 75, window=ent1)
 
-tk.Entry(root).grid(row=1, column=1)
-tk.Entry(root).grid(row=2, column=1)
+
+lbl2 = tk.Label(root, text="Last name")
+canvas1.create_window(100, 100, window=lbl2)
+ent2 = tk.Entry(root)
+canvas1.create_window(200, 100, window=ent2)
 
 
 button = tk.Button(root, text="Exit", width=25, command=root.destroy)
-button.grid(row=3, column=0, columnspan=2, pady=10)
+canvas1.create_window(640, 200, window=button)
 
 var1 = tk.IntVar()
 var2 = tk.IntVar()
-
-tk.Checkbutton(root, text="Male", variable=var1).grid(row=0, sticky="w")
-tk.Checkbutton(root, text="Female", variable=var2).grid(row=1, sticky=tk.W)
-
-
-
 
 root.mainloop()
