4b9a101e65932bf016133f8958b2827c1bda2ba1
23365 <23365@students.mrgs.school.nz>
2026-05-08 12:32:36 +1200

    Adding a new page testing

--- HomePage.py
+++ HomePage.py
@@ -1,6 +1,25 @@
 import tkinter as tk
 from tkinter import *
 
+from test import canvas1
+
+
+def start_quiz():
+    canvas1.delete("all")
+
+question1_lbl = tk.Label(root, text="Question 1: Where is the Taj Mahal?", fg="black", bg="#fcf4ea", font=("Verdana", 18, "bold") )
+canvas1.create_window(600, 100, window=homepage.btn)
+
+
+
+
+
+
+
+
+
+
+
 root = tk.Tk()
 root.title("7 Wonders Of The World Quiz")
 root.geometry("1207x675")
@@ -10,9 +29,11 @@ canvas1 = Canvas(root, width=1204, height=680)
 canvas1.pack(fill="both", expand=True)
 canvas1.create_image(0, 0, image=bg, anchor="nw")
 
+#Labels and Entries
+
 lbl1 = tk.Label(root, text="First name",
                 fg="black",
-                bg="#fce6b4",
+                bg="#fcf4ea",
                 font=("Verdana", 14, "bold"))
 
 canvas1.create_window(67, 100, window=lbl1)
@@ -22,30 +43,27 @@ canvas1.create_window(200, 100, window=ent1)
 
 lbl2 = tk.Label(root, text="Last name",
                 fg="black",
-                bg="#fce6b4",
+                bg="#fcf4ea",
                 font=("Verdana", 14, "bold"))
 canvas1.create_window(67, 130, window=lbl2)
 ent2 = tk.Entry(root, width=20)
 canvas1.create_window(200, 130, window=ent2)
 
 
-button = tk.Button(root, text="Exit", width=25, command=root.destroy)
-canvas1.create_window(1050, 635, window=button)
-button.config(background= "#fce6b4", height=1, width=5, font=("Verdana", 24, "bold"))
+#Buttons
+
+button = tk.Button(root, text="Exit", width=25, command=root.destroy, relief="flat", overrelief="raised", background="#fcbe97")
+canvas1.create_window(825, 610, window=button)
+button.config(height=1, width=5, font=("Verdana", 24, "bold"))
 
 
-Start_button = tk.Button(root, text="Start Quiz", width=20, command=root.destroy)
-canvas1.create_window(602, 600, window=Start_button)
-button.config(background= "#fce6b4", height=1, width=5, font=("Verdana", 24, "bold"))
-
-
-
+Start_button = tk.Button(root, text="Start Quiz", relief="flat", overrelief="raised", background="#fcbe97", command=lambda: show_frame(page1) )
+canvas1.create_window(365, 610, window=Start_button)
+Start_button.config(font=("Verdana", 24, "bold") )
 
 var1 = tk.IntVar()
 var2 = tk.IntVar()
 
-
-#Spend next Lesson to make a button that moves to the next quiz page :)
 
 
 root.mainloop()
